aced69d1cc186fca13b2a981222456b28a19585a
Ivanski1024 <ivan.antonov.p@gmail.com>
2014-11-09 05:05:49 +0200

    src base

---
+++ source/index.js
@@ -0,0 +1,17 @@
+
+
+var modules;
+
+(function() {
+  init(config);
+})
+
+exports.init = function () {
+  modules = require('./modules.js');
+},
+
+exports.getModules = function() {
+  if(modules) {
+
+  }
+},
